24522eb84f3af284747c0e21441fc775d2331ed2
Nuraly <iman.zhsp@mail.ru>
2025-10-11 22:25:56 +0500

    new

--- carts/admin.py
+++ carts/admin.py
@@ -4,13 +4,13 @@ from .models import Cart, CartItem
 
 class CartAdmin(admin.ModelAdmin):
     list_display = ('cart_id', 'date_added')
-    list_per_page = 20
+    list_per_page = 25
     list_max_show_all = 100
 
 
 class CartItemAdmin(admin.ModelAdmin):
     list_display = ('product', 'cart', 'quantity', 'is_active')
-    list_per_page = 20
+    list_per_page = 25
     list_max_show_all = 100
 
 
